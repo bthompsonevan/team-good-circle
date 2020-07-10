9d87dbea834e9f85bb3724f7df9930016d216542
bthompsonevan <bthompson.evan@gmail.com>
2020-07-10 15:33:31 -0700

    setting up folder system for sass

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
+  <div class="home-page-container">
     Home
+
+    <div class="home-into-paragraph-container">
+      Good Circle is the programming team from the University of Central
+      Florida. Please explore and see how you can help us to keep attending
+      competitions.
+    </div>
   </div>
 </template>
 
