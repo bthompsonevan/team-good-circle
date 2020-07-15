75a39cc86760186639070de7c04bdd864e853a5b
bthompsonevan <bthompson.evan@gmail.com>
2020-07-15 13:55:30 -0700

    experimenting with colors and hover

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nav-container">
-    <b-nav tabs fill>
+    <b-nav nav nav-tabs fill>
       <b-nav-item class="nav-bar-link">Home</b-nav-item>
       <b-nav-item class="nav-bar-link">Upcoming Events</b-nav-item>
       <b-nav-item class="nav-bar-link">How To Help</b-nav-item>
