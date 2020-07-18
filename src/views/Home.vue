8c9fafa0abf3ccb1c4389646cf125f8dd3a677c9
bthompsonevan <bthompson.evan@gmail.com>
2020-07-18 11:12:28 -0700

    experimenting with column layouts on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,19 +7,28 @@
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <body class="home-page-container">
+      <header>
+        <h1>This is the Header</h1>
+      </header>
       <NavBar />
       <main>
-        <div class="home-into-paragraph-container">
-          Good Circle is the programming team from the University of Central
-          Florida. Please explore and see how you can help us to keep attending
-          competitions.
-        </div>
-        <div>
-          <img
-            class="home-logo-container"
-            src="@/assets/Team_Good_Circle_Log.png"
-            alt="Good Cirlce Logo"
-          />
+        <div class="row">
+          <div class="col"></div>
+          <div class="col">
+            <div class="home-into-paragraph-container">
+              Good Circle is the programming team from the University of Central
+              Florida. Please explore and see how you can help us to keep
+              attending competitions.
+            </div>
+            <div>
+              <img
+                class="home-logo-container"
+                src="@/assets/Team_Good_Circle_Log.png"
+                alt="Good Cirlce Logo"
+              />
+            </div>
+          </div>
+          <div class="col"></div>
         </div>
       </main>
       <footer class="home-footer-container">
