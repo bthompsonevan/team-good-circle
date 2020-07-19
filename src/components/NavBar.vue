914cb24386da9f2450a7fc0ece24e7c6f118fd86
bthompsonevan <bthompson.evan@gmail.com>
2020-07-19 11:44:50 -0700

    added router links for home and howtohelp navbar links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <b-nav nav nav-tabs fill class="nav-bar-container">
-      <b-nav-item class="nav-bar-link">Home</b-nav-item>
+      <b-nav-item class="nav-bar-link">
+        <router-link to="/">
+          Home
+        </router-link>
+      </b-nav-item>
       <b-nav-item class="nav-bar-link">Upcoming Events</b-nav-item>
-      <b-nav-item class="nav-bar-link">How To Help</b-nav-item>
+      <b-nav-item class="nav-bar-link">
+        <router-link to="/howtohelp">
+          How To Help
+        </router-link>
+      </b-nav-item>
     </b-nav>
   </div>
 </template>
